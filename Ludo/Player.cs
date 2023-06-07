8606fe12b68a5505c8d38aee6a5ad68c10c39232
Adam <adamardiyantow@gmail.com>
2023-06-07 10:14:41 +0700

    create Player class and Iplayer

---
+++ Ludo/Player.cs
@@ -0,0 +1,11 @@
+namespace LudoApp;
+
+public class Player : IPlayer
+{
+    private int _id;
+    private string _name;
+
+    
+    public int ID { get; set; }
+    public string Name { get; set; }
+}
