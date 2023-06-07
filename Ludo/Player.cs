becdd35514ad6327497b35ed5a3f7374a9595d10
Adam <adamardiyantow@gmail.com>
2023-06-07 10:32:59 +0700

    create pawn class and IPawn interface

--- Ludo/Player.cs
+++ Ludo/Player.cs
@@ -2,10 +2,12 @@ namespace LudoApp;
 
 public class Player : IPlayer
 {
-    private int _id;
-    private string _name;
-
-    
     public int ID { get; set; }
     public string Name { get; set; }
+
+    public Player(int id, string name)
+    {
+        this.Name = name;
+        this.ID = id;
+    }
 }
