0084b2f913cd1c109088e67b9d58fe355d87cdb7
Adam <adamardiyantow@gmail.com>
2023-06-08 20:22:50 +0700

    create method movepawn in GameRunner

--- Ludo/GameRunner.cs
+++ Ludo/GameRunner.cs
@@ -1,16 +1,16 @@
 namespace LudoApp;
 public class GameRunner
 {
-    private Board board;
+    private Board _board;
     private IDice dice = new Dice();
     private Dictionary<IPlayer, string> _players;
-    private Dictionary<IPlayer, IPawn> pawns;
+    private Dictionary<IPlayer, IPawn> _pawns;
     private IPlayer _currentPlayer;
     private List<IPlayer> _playersOrder = new List<IPlayer>();
     
     public void CreateBoard(List<int> safeCells, Dictionary<string, int> homeCells, Dictionary<string, int> startCells)
     {
-        board = new(safeCells, homeCells, startCells); // instantiate board?
+        _board = new(safeCells, homeCells, startCells); // instantiate board?
     }
     public void AddPlayer(IPlayer player, string color)
     {
@@ -50,14 +50,14 @@ public class GameRunner
     }
     public bool CheckIsSafeCell(int position)
     {
-        return board.GetSafeCells().Contains(position); // return true if cell is safe
+        return _board.GetSafeCells().Contains(position); // return true if cell is safe
     }
     public void PawnToStart(IPawn pawn, string color)
     {
-        int startPos = board.GetStartCells()[color]; // get the start position from board
+        int startPos = _board.GetStartCells()[color]; // get the start position from board
         pawn.SetPosition(startPos);// set pawn position to start by its color
     }
-    public void PawnToBase(IPawn pawn, string color)
+    public void PawnToBase(IPawn pawn)
     {
         pawn.SetPosition(0); // set pawn position to 0 or to base
     }
@@ -67,6 +67,36 @@ public class GameRunner
     }
     public void MovePawn(IPawn pawn, int step)
     {
-
+        int position = pawn.GetPosition();
+        if(_board.GetHomeCells().ContainsValue(position))
+        {
+            // move to colored cell
+            pawn.SetPosition(53);
+            MovePawn(pawn, step-1);
+        }
+        if(position == 52) //
+        {
+            pawn.SetPosition(1); //restart position to 1
+        }else
+        {
+            pawn.SetPosition(position + 1); // move pawn to next position
+        }
+        step--;
+        if (step != 0)
+        {
+            MovePawn(pawn, step);
+        }
+        foreach (var kvp in _pawns)
+        {
+            // check if there is another pawn in cell
+            if (kvp.Value.GetPosition() == pawn.GetPosition())
+            {
+                // check if it is not the same pawn and not in safe cell
+                if (kvp.Key != pawn.GetPlayer() && !CheckIsSafeCell(pawn.GetPosition()))
+                {
+                    PawnToBase(kvp.Value); //send previous pawn to base
+                }
+            }
+        }
     }
 }
