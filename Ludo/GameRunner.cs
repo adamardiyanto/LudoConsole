5ec74d84fae40fc7d333381685f7415088ba1c18
Adam <adamardiyantow@gmail.com>
2023-06-08 08:24:22 +0700

    define enum color and create var for GameRunner

---
+++ Ludo/GameRunner.cs
@@ -0,0 +1,9 @@
+namespace LudoApp;
+public class GameRunner
+{
+    private IBoard board;
+    private IDice dice;
+    private Dictionary<IPlayer, string> players;
+    private Dictionary<IPlayer, IPawn> pawns;
+    
+}
