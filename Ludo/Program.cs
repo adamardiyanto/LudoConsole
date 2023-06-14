20a1c73e1feb0c94448d44eea509b3508fc1ad67
adamardiyanto <adamardiyantow@gmail.com>
2023-06-14 17:02:15 +0700

    more method in program.cs

--- Ludo/Program.cs
+++ Ludo/Program.cs
@@ -1,15 +1,24 @@
 ﻿namespace LudoApp;
 
-public static class Program
+public class Program
 {
+    List<int> _safeCell = new List<int>(){1,9,14, 22, 27, 35, 40, 48};
+    static Dictionary<string, int> _startCell = new Dictionary<string, int>();
+    static Dictionary<string, int> _homeCell = new Dictionary<string, int>();
+    static GameRunner _runner;
+
     public static void Main(string[] args)
     {
-        private List<int> _safeCell = new List<int>(1,9,14, 22, 27, 35, 40, 48);
-        private Dictionary<string, int> _startCell = new Dictionary<string, int>();
         _startCell.Add(Color.Red.ToString(),1);
         _startCell.Add(Color.Green.ToString(),14);
         _startCell.Add(Color.Blue.ToString(),27);
         _startCell.Add(Color.Yellow.ToString(),40);
+
+        _homeCell.Add(Color.Red.ToString(),51);
+        _homeCell.Add(Color.Green.ToString(),12);
+        _homeCell.Add(Color.Blue.ToString(),25);
+        _homeCell.Add(Color.Yellow.ToString(),38);
+
 
         Dice dice = new();
         Console.WriteLine(dice.Roll());
