24c434ee4ea2afde228f35bbe4e25f462ecffa38
adam ardiyanto <adamardiyantow@gmail.com>
2023-06-21 15:44:45 +0700

    Revert "separate method in gamerunner to preogram"
    This reverts commit 1902a658017fcbf6c8bc5c1901ed750d1fc5c1b5.

--- Ludo/Program.cs
+++ Ludo/Program.cs
@@ -10,9 +10,8 @@ public class Program
     static private Board _board;
     static GameRunner _runner;
     static Player[] player = new Player[4];
-    static int numberOfPlayer;
-    static private int _numPawn;
-    static IPlayer _currentPlayer;
+    static List<IPlayer> tempListPlayer = new List<IPlayer>();
+    static int numberOfPlayer = 0;
 
     public static void Main(string[] args)
     {
@@ -40,18 +39,23 @@ public class Program
             Console.WriteLine("player {0} please enter your name : ", i + 1);
             string? name = Console.ReadLine();
             player[i] = new Player(i, name);
+            //tempListPlayer.Add(player[i]);
+            //temporary direct asign order and color
             _playerList.Add(player[i], (Color)i); // add player to list
         }
+        //create GameRunner
+        Console.WriteLine("creating GameRunner...");
+        _runner = new GameRunner(_board, _playerList);
         //create pawn
+        Console.WriteLine("Creating pawn...");
         foreach (var p in _playerList)
         {
             _pawnList.Add(p.Key, _runner.CreatePawn());
         }
-        _runner = new GameRunner(_board, _playerList);
         _runner.SetPawnList(_pawnList);
 
         //start the game
-        StartGame();
+        _runner.StartGame();
         List<IPlayer> winners = _runner.GetWinners();
         Console.WriteLine("winner player : ");
         foreach (IPlayer winner in winners)
@@ -64,128 +68,6 @@ public class Program
         if (int.TryParse(Console.ReadLine(), out numberOfPlayer))
         {
             return numberOfPlayer > 1 && numberOfPlayer <= 4;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    static public void StartGame()
-    {
-        _runner.clearBoard();
-        _runner.updateBoard(_pawnList, _playerList);
-        _runner.showBoard();
-        while (!_runner.CheckEndGame())
-        {
-            foreach (var player in _playerList) // looping each player
-            {
-                _currentPlayer = player.Key;
-                int diceValue = 0;
-                do // looping if player get 6
-                {
-                    Console.WriteLine(player.Key.Name.ToUpper() + " press enter to roll dice");
-                    Console.ReadLine();
-                    diceValue = _runner.RollDice();
-                    Console.WriteLine(diceValue);
-                    Console.ReadLine();
-                    if ( _runner.CountPawnOutOfBase(_currentPlayer) is 0) // there are no pawn out of base
-                    {
-                        if (_runner.CheckIsSix(diceValue))
-                        {
-                            _runner.PawnToStart(_pawnList[_currentPlayer][0], _playerList[_currentPlayer]);
-                        }
-                    }
-                    else if (_runner.CountPawnOutOfBase(_currentPlayer) is 1) // if there is a pawn out of base
-                    {
-                        if (_runner.CheckIsSix(diceValue))
-                        {
-                            MoveOrOut(diceValue,_runner.CountPawnOutOfBase(_currentPlayer));
-                        }
-                        else
-                        {
-                            _runner.MovePawn(_pawnList[_currentPlayer].Find(x => x.GetPosition() > (int)Cell.Base && x.GetPosition() < (int)Cell.Triangle), diceValue);
-                        }
-                    }
-                    else if (_runner.CountPawnOutOfBase(_currentPlayer) < 4)
-                    {
-                        if (_runner.CheckIsSix(diceValue))
-                        {
-                            MoveOrOut(diceValue, _runner.CountPawnOutOfBase(_currentPlayer));
-                        }
-                        else
-                        {
-                            SelectPawnToMove(diceValue);
-                        }
-
-                    }
-                    else
-                    {
-                        SelectPawnToMove(diceValue);
-
-                    }
-                    _runner.clearBoard();
-        _runner.updateBoard(_pawnList, _playerList);
-        _runner.showBoard();
-                } while (diceValue == 6);
-
-            }
-        }
-    }
-    static private void MoveOrOut(int diceValue, int pawnOutbase)
-    {
-        Console.WriteLine("choose pawn out of base or move pawn");
-        Console.WriteLine("press M to move pawn and press O to release pawn");
-
-        while (true)
-        {
-            string input = Console.ReadLine();
-            if (input == "m" || input == "M")
-            {
-                if (pawnOutbase == 1)
-                {
-                    _runner.MovePawn(_pawnList[_currentPlayer].Find(x => x.GetPosition() > (int)Cell.Base && x.GetPosition() < (int)Cell.Triangle), diceValue);
-                    break;
-                }
-                else
-                {
-                    SelectPawnToMove(diceValue);
-                    break;
-                }
-            }
-            else if (input == "o" || input == "O")
-            {
-                IPawn p = _pawnList[_currentPlayer].Find(x => x.GetPosition() is (int)Cell.Base);
-                _runner.PawnToStart(p, _playerList[_currentPlayer]);
-                break;
-            }
-            else
-            {
-                Console.WriteLine("Invalid input");
-            }
-        }
-    }
-
-    static private void SelectPawnToMove(int diceValue)
-    {
-        // select pawn to move
-        List<IPawn> listPawns = _pawnList[_currentPlayer].FindAll(x => x.GetPosition() > (int)Cell.Base && x.GetPosition() < (int)Cell.Triangle);
-        foreach (IPawn p in listPawns)
-        {
-            Console.WriteLine((listPawns.IndexOf(p) + 1) + ". pawn with position " + p.GetPosition());
-        }
-        Console.WriteLine("select pawn to move");
-        Console.WriteLine("enter number based on pawn order");
-        while (!ValidatePawn(listPawns.Count))
-        {
-            Console.WriteLine("Invalid input or number out of range");
-        }
-        _runner.MovePawn(listPawns[_numPawn - 1], diceValue);
-    }
-    static private bool ValidatePawn(int totalPawn)
-    {
-        if (int.TryParse(Console.ReadLine(), out _numPawn))
-        {
-            return _numPawn > 0 && _numPawn <= totalPawn;
         }
         else
         {
